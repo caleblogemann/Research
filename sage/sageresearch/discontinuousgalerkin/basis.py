d4fbcc2ba7568e33feb26c306f566514da234a55
Caleb Logemann <logemann@iastate.edu>
2021-09-29 15:04:25 -0500

    updated sage for dg

--- sage/sageresearch/discontinuousgalerkin/basis.py
+++ sage/sageresearch/discontinuousgalerkin/basis.py
@@ -2,6 +2,31 @@ from sageresearch.utils import legendre_polynomials
 
 import sage.all as sa
 
+one = sa.Integer(1)
+two = sa.Integer(2)
+three = sa.Integer(3)
+four = sa.Integer(4)
+five = sa.Integer(5)
+six = sa.Integer(6)
+seven = sa.Integer(7)
+eight = sa.Integer(8)
+nine = sa.Integer(9)
+eleven = sa.Integer(11)
+fifteen = sa.Integer(15)
+sixteen = sa.Integer(16)
+eighteen = sa.Integer(18)
+nineteen = sa.Integer(19)
+twenty_one = sa.Integer(21)
+twenty_seven = sa.Integer(27)
+thirty_three = sa.Integer(33)
+thirty_five = sa.Integer(35)
+thirty_nine = sa.Integer(39)
+forty_five = sa.Integer(45)
+one_hundred_five = sa.Integer(105)
+one_thirty_five = sa.Integer(135)
+
+
+
 
 def get_nodal_basis_1d(nodes):
     pass
@@ -15,14 +40,164 @@ def get_gauss_lobatto_nodal_basis_1d(space_order):
     pass
 
 
-def get_legendre_basis_1d(space_order, inner_product_constant=0.5):
-    return legendre_polynomials.get_legendre_polynomials(space_order, inner_product_constant)
+def get_legendre_basis_1d(space_order, inner_product_constant=one / two):
+    return legendre_polynomials.get_legendre_polynomials(
+        space_order, inner_product_constant
+    )
 
 
-def get_legendre_basis_2d_rectangular(space_order, inner_product_constant=0.25):
-    phi = legendre_polynomials.get_legendre_polynomials(space_order, inner_product_constant)
+def get_legendre_basis_2d_rectangular(space_order, inner_product_constant=one / four):
+    phi = legendre_polynomials.get_legendre_polynomials(
+        space_order, inner_product_constant
+    )
     return phi
 
 
 def get_mass_matrix_1d(basis):
     pass
+
+
+def get_modal_basis_2d_triangle(space_order, inner_product_constant=one / two):
+    pass
+
+
+def get_modal_basis_coefficients_2d_triangle(
+    space_order, inner_product_constant=one / two
+):
+    # coefficients of basis functions in terms on monomials
+    # ordering 1, xi, eta, xi^2, xi * eta, eta^2, xi^3, xi^2 * eta, eta^2 * xi, ...
+    # Done up to fourth degree monomials works for up to 5th order in space
+    # coefficients are for polynomials orthonormal with inner product constant of
+    # 0.5
+
+    sqrt2 = sa.sqrt(two)
+    sqrt3 = sa.sqrt(three)
+    sqrt5 = sa.sqrt(five)
+    sqrt6 = sa.sqrt(six)
+    sqrt7 = sa.sqrt(seven)
+    sqrt15 = sa.sqrt(fifteen)
+    sqrt35 = sa.sqrt(thirty_five)
+
+    coeffs = sa.matrix(15, 15)
+    coeffs[0, 0] = one
+
+    coeffs[1, 0] = sqrt2 / two
+    coeffs[1, 1] = sqrt2 * three / two
+
+    coeffs[2, 0] = sqrt6 / two
+    coeffs[2, 1] = sqrt6 / two
+    coeffs[2, 2] = sqrt6
+
+    coeffs[3, 0] = -sqrt3 / two
+    coeffs[3, 1] = sqrt3
+    coeffs[3, 3] = sqrt3 * five / two
+
+    coeffs[4, 0] = nine / four
+    coeffs[4, 1] = six
+    coeffs[4, 2] = nine / two
+    coeffs[4, 3] = fifteen / four
+    coeffs[4, 4] = fifteen / two
+
+    coeffs[5, 0] = sqrt15 / four
+    coeffs[5, 1] = sqrt15
+    coeffs[5, 2] = sqrt15 * three / two
+    coeffs[5, 3] = sqrt15 / four
+    coeffs[5, 4] = sqrt15 * three / two
+    coeffs[5, 5] = sqrt15 * three / two
+
+    coeffs[6, 0] = -three / four
+    coeffs[6, 1] = -fifteen / four
+    coeffs[6, 3] = fifteen / four
+    coeffs[6, 6] = thirty_five / four
+
+    coeffs[7, 0] = sqrt3 / four
+    coeffs[7, 1] = sqrt3 * nineteen / four
+    coeffs[7, 2] = sqrt3 / two
+    coeffs[7, 3] = sqrt3 * thirty_nine / four
+    coeffs[7, 4] = sqrt3 * nine
+    coeffs[7, 6] = sqrt3 * twenty_one / four
+    coeffs[7, 7] = sqrt3 * twenty_one / two
+
+    coeffs[8, 0] = sqrt5 * five / four
+    coeffs[8, 1] = sqrt5 * twenty_seven / four
+    coeffs[8, 2] = sqrt5 * fifteen / two
+    coeffs[8, 3] = sqrt5 * thirty_three / four
+    coeffs[8, 4] = sqrt5 * eighteen
+    coeffs[8, 5] = sqrt5 * fifteen / two
+    coeffs[8, 6] = sqrt5 * seven / four
+    coeffs[8, 7] = sqrt5 * twenty_one / two
+    coeffs[8, 8] = sqrt5 * twenty_one / two
+
+    coeffs[9, 0] = sqrt7 / four
+    coeffs[9, 1] = sqrt7 * nine / four
+    coeffs[9, 2] = sqrt7 * three
+    coeffs[9, 3] = sqrt7 * nine / four
+    coeffs[9, 4] = sqrt7 * nine
+    coeffs[9, 5] = sqrt7 * fifteen / two
+    coeffs[9, 6] = sqrt7 / four
+    coeffs[9, 7] = sqrt7 * three
+    coeffs[9, 8] = sqrt7 * fifteen / two
+    coeffs[9, 9] = sqrt7 * five
+
+    coeffs[10, 0] = sqrt5 * three / eight
+    coeffs[10, 1] = sqrt5 * -three / two
+    coeffs[10, 3] = sqrt5 * -twenty_one / four
+    coeffs[10, 6] = sqrt5 * seven / two
+    coeffs[10, 10] = sqrt5 * sa.Integer(63) / eight
+
+    coeffs[11, 0] = sqrt15 / -two
+    coeffs[11, 1] = sqrt15 / -two
+    coeffs[11, 2] = sqrt15 * -one
+    coeffs[11, 3] = sqrt15 * twenty_one / four
+    coeffs[11, 6] = sqrt15 * twenty_one / two
+    coeffs[11, 7] = sqrt15 * twenty_one / two
+    coeffs[11, 10] = sqrt15 * twenty_one / four
+    coeffs[11, 11] = sqrt15 * twenty_one / two
+
+    coeffs[12, 0] = five / two
+    coeffs[12, 1] = forty_five / two
+    coeffs[12, 2] = fifteen
+    coeffs[12, 3] = sa.Integer(255) / four
+    coeffs[12, 4] = sa.Integer(90)
+    coeffs[12, 5] = fifteen
+    coeffs[12, 6] = sa.Integer(115) / two
+    coeffs[12, 7] = sa.Integer(285) / two
+    coeffs[12, 8] = sa.Integer(75)
+    coeffs[12, 10] = forty_five / four
+    coeffs[12, 11] = one_thirty_five / two
+    coeffs[12, 12] = one_thirty_five / two
+
+    coeffs[13, 0] = sqrt35 * seven / sixteen
+    coeffs[13, 1] = sqrt35 * nine / two
+    coeffs[13, 2] = sqrt35 * twenty_one / four
+    coeffs[13, 3] = sqrt35 * nine
+    coeffs[13, 4] = sqrt35 * forty_five / two
+    coeffs[13, 5] = sqrt35 * one_hundred_five / eight
+    coeffs[13, 6] = sqrt35 * eleven / two
+    coeffs[13, 7] = sqrt35 * sa.Integer(51) / two
+    coeffs[13, 8] = sqrt35 * sa.Integer(30)
+    coeffs[13, 9] = sqrt35 * thirty_five / four
+    coeffs[13, 10] = sqrt35 * nine / sixteen
+    coeffs[13, 11] = sqrt35 * twenty_seven / four
+    coeffs[13, 12] = sqrt35 * one_thirty_five / eight
+    coeffs[13, 13] = sqrt35 * forty_five / four
+
+    coeffs[14, 0] = sqrt5 * three / sixteen
+    coeffs[14, 1] = sqrt5 * three
+    coeffs[14, 2] = sqrt5 * fifteen / four
+    coeffs[14, 3] = sqrt5 * twenty_seven / four
+    coeffs[14, 4] = sqrt5 * forty_five / two
+    coeffs[14, 5] = sqrt5 * one_thirty_five / eight
+    coeffs[14, 6] = sqrt5 * three
+    coeffs[14, 7] = sqrt5 * forty_five / two
+    coeffs[14, 8] = sqrt5 * forty_five
+    coeffs[14, 9] = sqrt5 * one_hundred_five / four
+    coeffs[14, 10] = sqrt5 * three / sixteen
+    coeffs[14, 11] = sqrt5 * fifteen / four
+    coeffs[14, 12] = sqrt5 * one_thirty_five / eight
+    coeffs[14, 13] = sqrt5 * one_hundred_five / four
+    coeffs[14, 14] = sqrt5 * one_hundred_five / eight
+
+    coeffs = coeffs / sa.sqrt(two * inner_product_constant)
+    num_basis_cpts = int(space_order * (space_order + 1) / 2)
+    return coeffs[:num_basis_cpts, :num_basis_cpts]
--- sage/sageresearch/discontinuousgalerkin/canonical_element.py
+++ sage/sageresearch/discontinuousgalerkin/canonical_element.py
@@ -1,62 +1,440 @@
 import sage.all as sa
 
+zero = sa.Integer(0)
+one = sa.Integer(1)
+two = sa.Integer(2)
+four = sa.Integer(4)
+
+volume_1d = two
+volume_2d_rectangle = four
+volume_2d_triangle = two
+
+vertices_1d = sa.matrix([[-one], [one]])
+vertices_2d_rectangle = sa.matrix([[-one, -one], [one, -one], [one, one], [-one, one]])
+vertices_2d_triangle = sa.matrix([[-one, one], [-one, -one], [one, -one]])
+
+
+def get_mesh_variables_1d():
+    x = sa.SR.symbol("x", domain="real")
+    return x
+
+
+def get_canonical_variables_1d():
+    xi = sa.SR.symbol("xi", domain="real")
+    return xi
+
+
+def integrate_over_mesh_element_1d(f, x_left, x_right):
+    # f should be symbolic expression of mesh variables, x
+    x = get_mesh_variables_1d()[0]
+    return sa.integrate(f, x, x_left, x_right)
+
+
+def integrate_over_canonical_element_1d(f, var):
+    # f should be symbolic expression of canonical variables, xi
+    xi = get_canonical_variables_1d()
+    return sa.integrate(f, xi, -1, 1)
+
+
 def transform_to_canonical_1d_vertex_list(x, vertex_list):
-    return transform_to_canonical_1d(
-        x, vertex_list[0, 0], vertex_list[1, 0]
-    )
+    return transform_to_canonical_1d(x, vertex_list[0, 0], vertex_list[1, 0])
+
 
 def transform_to_canonical_1d(x, x_left, x_right):
     # xi = (x - x_c) 2 / delta_x
-    x_c = 0.5 * (x_left + x_right)
+    x_c = (x_left + x_right) / two
     delta_x = x_right - x_left
-    xi = (x - x_c) * 2.0 / delta_x
+    xi = (x - x_c) * two / delta_x
     return xi
 
+
 def transform_to_canonical_jacobian_1d_vertex_list(vertex_list):
-    return transform_to_canonical_jacobian_1d(
-        vertex_list[0, 0], vertex_list[1, 0]
-    )
+    return transform_to_canonical_jacobian_1d(vertex_list[0, 0], vertex_list[1, 0])
+
 
 def transform_to_canonical_jacobian_1d(x_left, x_right):
     delta_x = x_right - x_left
-    return sa.matrix([[2.0 / delta_x]])
+    return sa.matrix([[two / delta_x]])
+
 
 def transform_to_canonical_jacobian_determinant_1d_vertex_list(vertex_list):
     return transform_to_canonical_jacobian_determinant_1d(
         vertex_list[0, 0], vertex_list[1, 0]
     )
 
+
 def transform_to_canonical_jacobian_determinant_1d(x_left, x_right):
     delta_x = x_right - x_left
-    return 2.0 / delta_x
+    return two / delta_x
+
 
 def transform_to_mesh_1d_vertex_list(xi, vertex_list):
-    return transform_to_mesh_1d(
-        xi, vertex_list[0, 0], vertex_list[1, 0]
-    )
+    return transform_to_mesh_1d(xi, vertex_list[0, 0], vertex_list[1, 0])
+
 
 def transform_to_mesh_1d(xi, x_left, x_right):
     # x = xi * delta_x / 2 + x_c
-    x_c = 0.5 * (x_left + x_right)
+    x_c = (x_left + x_right) / two
     delta_x = x_right - x_left
-    x = xi * delta_x * 0.5 + x_c
+    x = xi * delta_x / two + x_c
     return x
 
+
 def transform_to_mesh_jacobian_1d_vertex_list(vertex_list):
-    return transform_to_mesh_jacobian_1d(
-        vertex_list[0, 0], vertex_list[1, 0]
-    )
+    return transform_to_mesh_jacobian_1d(vertex_list[0, 0], vertex_list[1, 0])
+
 
 def transform_to_mesh_jacobian_1d(x_left, x_right):
     delta_x = x_right - x_left
-    return sa.matrix([[delta_x * 0.5]])
+    return sa.matrix([[delta_x / two]])
+
 
 def transform_to_mesh_jacobian_determinant_1d_vertex_list(vertex_list):
     return transform_to_mesh_jacobian_determinant_1d(
         vertex_list[0, 0], vertex_list[1, 0]
     )
 
+
 def transform_to_mesh_jacobian_determinant_1d(x_left, x_right):
     delta_x = x_right - x_left
-    return 0.5 * delta_x
+    return delta_x / two
 
+
+def get_mesh_variables_2d():
+    x = sa.SR.symbol("x", domain="real")
+    y = sa.SR.symbol("y", domain="real")
+    return (x, y)
+
+
+def get_canonical_variables_2d():
+    xi = sa.SR.symbol("xi", domain="real")
+    eta = sa.SR.symbol("eta", domain="real")
+    return (xi, eta)
+
+
+def integrate_over_mesh_element_2d_rectangle(f, x_left, x_right, y_bottom, y_top):
+    tuple_ = get_mesh_variables_2d()
+    x = tuple_[0]
+    y = tuple_[1]
+    integral = sa.integrate(sa.integrate(f, x, x_left, x_right), y, y_bottom, y_top)
+    return integral
+
+def integrate_over_canonical_element_2d_rectangle(f):
+    tuple_ = get_canonical_variables_2d()
+    xi = tuple_[0]
+    eta = tuple_[1]
+    integral = sa.integrate(sa.integrate(f, xi, -one, one), eta, -one, one)
+    return integral
+
+def transform_to_canonical_2d_rectangle_vertex_list(x, vertex_list):
+    # vertex_list should be in order, bottom_left, bottom_right, top_right, top_left
+    return transform_to_canonical_2d_rectangle_interval(
+        x, vertex_list[0, 0], vertex_list[1, 0], vertex_list[0, 1], vertex_list[3, 1],
+    )
+
+
+def transform_to_canonical_2d_rectangle_interval(x, x_left, x_right, y_bottom, y_top):
+    # x should either be shape (2,) or (2, num_points)
+    # xi = (x - x_c) * 2.0 / delta_x
+    # eta = (y - y_c) * 2.0 / delta_x
+    x_c = (x_left + x_right) / two
+    y_c = (y_bottom + y_top) / two
+    delta_x = x_right - x_left
+    delta_y = y_top - y_bottom
+    xi = (x[0] - x_c) * two / delta_x
+    eta = (x[1] - y_c) * two / delta_y
+    return sa.vector([xi, eta])
+
+
+def transform_to_canonical_jacobian_2d_rectangle_vertex_list(vertex_list):
+    return transform_to_canonical_jacobian_2d_rectangle_interval(
+        vertex_list[0, 0], vertex_list[1, 0], vertex_list[0, 1], vertex_list[3, 1]
+    )
+
+
+def transform_to_canonical_jacobian_2d_rectangle_interval(
+    x_left, x_right, y_bottom, y_top
+):
+    delta_x = x_right - x_left
+    delta_y = y_top - y_bottom
+    return sa.matrix([[two / delta_x, 0.0], [0, two / delta_y]])
+
+
+def transform_to_canonical_jacobian_determinant_2d_rectangle_vertex_list(vertex_list):
+    return transform_to_canonical_jacobian_determinant_2d_rectangle_interval(
+        vertex_list[0, 0], vertex_list[1, 0], vertex_list[0, 1], vertex_list[3, 1]
+    )
+
+
+def transform_to_canonical_jacobian_determinant_2d_rectangle_interval(
+    x_left, x_right, y_bottom, y_top
+):
+    delta_x = x_right - x_left
+    delta_y = y_top - y_bottom
+    return four / (delta_x * delta_y)
+
+
+def transform_to_mesh_2d_rectangle_vertex_list(xi, vertex_list):
+    return transform_to_mesh_2d_rectangle_interval(
+        xi, vertex_list[0, 0], vertex_list[1, 0], vertex_list[0, 1], vertex_list[3, 1],
+    )
+
+
+def transform_to_mesh_2d_rectangle_interval(xi, x_left, x_right, y_bottom, y_top):
+    # xi should either be shape (2,) or (num_points, 2)
+    x_c = (x_left + x_right) / two
+    y_c = (y_bottom + y_top) / two
+    delta_x = x_right - x_left
+    delta_y = y_top - y_bottom
+    x = xi[0] * delta_x / two + x_c
+    y = xi[1] * delta_y / two + y_c
+    return sa.vector([x, y])
+
+
+def transform_to_mesh_jacobian_2d_rectangle_vertex_list(vertex_list):
+    return transform_to_mesh_jacobian_2d_rectangle_interval(
+        vertex_list[0, 0], vertex_list[1, 0], vertex_list[0, 1], vertex_list[3, 1]
+    )
+
+
+def transform_to_mesh_jacobian_2d_rectangle_interval(x_left, x_right, y_bottom, y_top):
+    delta_x = x_right - x_left
+    delta_y = y_top - y_bottom
+    return sa.matrix([[delta_x / two, 0], [0, delta_y / two]])
+
+
+def transform_to_mesh_jacobian_determinant_2d_rectangle_vertex_list(vertex_list):
+    return transform_to_mesh_jacobian_determinant_2d_rectangle_interval(
+        vertex_list[0, 0], vertex_list[1, 0], vertex_list[0, 1], vertex_list[3, 1]
+    )
+
+
+def transform_to_mesh_jacobian_determinant_2d_rectangle_interval(
+    x_left, x_right, y_bottom, y_top
+):
+    delta_x = x_right - x_left
+    delta_y = y_top - y_bottom
+    return (delta_x * delta_y) / four
+
+
+def integrate_over_mesh_element_2d_triangle(f, vertex_list):
+    tuple_ = get_mesh_variables_2d()
+    x = tuple_[0]
+    y = tuple_[1]
+
+    sorted_vertex_list = vertex_list.copy()
+    sorted_vertex_list.sort()
+
+    x_left = sorted_vertex_list[0, 0]
+    x_middle = sorted_vertex_list[1, 0]
+    x_right = sorted_vertex_list[2, 0]
+    # if middle vertex higher than right vertex
+    if sorted_vertex_list[1, 1] > sorted_vertex_list[2, 1]:
+        y_bottom =
+        y_top =
+    else:
+
+    left_integral = sa.integrate(sa.integrate(f, y, ), x, x_left, x_middle)
+    right_integral = sa.integrate(sa.integrate(f, y, ), x, x_middle, x_right)
+
+    return left_integral + right_integral
+
+
+
+def integrate_over_canonical_element_2d_triangle(f):
+    tuple_ = get_canonical_variables_2d()
+    xi = tuple_[0]
+    eta = tuple_[1]
+    integral = sa.integrate(sa.integrate(f, xi, -one, -eta), eta, -one, one)
+    return integral
+
+
+def transform_to_canonical_2d_triangle_vertex_list(x, vertex_list):
+    # x.shape = (2, points.shape)
+    # vertex_list = (3, 2)
+    # return shape (2, points.shape)
+    # xi[0] = a_0_0 x[0] + a_0_1 x[1] + a_0_2
+    # xi[1] = a_1_0 x[0] + a_1_1 x[1] + a_1_2
+    x_0 = vertex_list[0]
+    x_1 = vertex_list[1]
+    x_2 = vertex_list[2]
+    a_0_0 = (
+        -2.0
+        * (x_0[1] - x_1[1])
+        / (
+            x_0[1] * (x_1[0] - x_2[0])
+            - x_0[0] * (x_1[1] - x_2[1])
+            + x_1[1] * x_2[0]
+            - x_1[0] * x_2[1]
+        )
+    )
+    a_0_1 = (
+        2.0
+        * (x_0[0] - x_1[0])
+        / (
+            x_0[1] * (x_1[0] - x_2[0])
+            - x_0[0] * (x_1[1] - x_2[1])
+            + x_1[1] * x_2[0]
+            - x_1[0] * x_2[1]
+        )
+    )
+    a_0_2 = (
+        x_0[1] * (x_1[0] + x_2[0])
+        - x_0[0] * (x_1[1] + x_2[1])
+        - x_1[1] * x_2[0]
+        + x_1[0] * x_2[1]
+    ) / (
+        x_0[1] * (x_1[0] - x_2[0])
+        - x_0[0] * (x_1[1] - x_2[1])
+        + x_1[1] * x_2[0]
+        - x_1[0] * x_2[1]
+    )
+    a_1_0 = (
+        -2.0
+        * (x_1[1] - x_2[1])
+        / (
+            x_0[1] * (x_1[0] - x_2[0])
+            - x_0[0] * (x_1[1] - x_2[1])
+            + x_1[1] * x_2[0]
+            - x_1[0] * x_2[1]
+        )
+    )
+    a_1_1 = (
+        2.0
+        * (x_1[0] - x_2[0])
+        / (
+            x_0[1] * (x_1[0] - x_2[0])
+            - x_0[0] * (x_1[1] - x_2[1])
+            + x_1[1] * x_2[0]
+            - x_1[0] * x_2[1]
+        )
+    )
+    a_1_2 = -(
+        x_0[1] * (x_1[0] - x_2[0])
+        - x_0[0] * (x_1[1] - x_2[1])
+        - x_1[1] * x_2[0]
+        + x_1[0] * x_2[1]
+    ) / (
+        x_0[1] * (x_1[0] - x_2[0])
+        - x_0[0] * (x_1[1] - x_2[1])
+        + x_1[1] * x_2[0]
+        - x_1[0] * x_2[1]
+    )
+
+    xi = sa.vector(
+        [a_0_0 * x[0] + a_0_1 * x[1] + a_0_2, a_1_0 * x[0] + a_1_1 * x[1] + a_1_2]
+    )
+    return xi
+
+
+def transform_to_canonical_jacobian_2d_triangle_vertex_list(vertex_list):
+    # jacobian of transformation to canonical
+    # should be a constant matrix as transformation is linear
+    # xi[0] = a_0_0 x[0] + a_0_1 x[1] + a_0_2
+    # xi[1] = a_1_0 x[0] + a_1_1 x[1] + a_1_2
+    # J = [[a_0_0, a_0_1], [a_1_0, a_1_1]]
+    x_0 = vertex_list[0]
+    x_1 = vertex_list[1]
+    x_2 = vertex_list[2]
+    a_0_0 = (
+        -two
+        * (x_0[1] - x_1[1])
+        / (
+            x_0[1] * (x_1[0] - x_2[0])
+            - x_0[0] * (x_1[1] - x_2[1])
+            + x_1[1] * x_2[0]
+            - x_1[0] * x_2[1]
+        )
+    )
+    a_0_1 = (
+        two
+        * (x_0[0] - x_1[0])
+        / (
+            x_0[1] * (x_1[0] - x_2[0])
+            - x_0[0] * (x_1[1] - x_2[1])
+            + x_1[1] * x_2[0]
+            - x_1[0] * x_2[1]
+        )
+    )
+    a_1_0 = (
+        -two
+        * (x_1[1] - x_2[1])
+        / (
+            x_0[1] * (x_1[0] - x_2[0])
+            - x_0[0] * (x_1[1] - x_2[1])
+            + x_1[1] * x_2[0]
+            - x_1[0] * x_2[1]
+        )
+    )
+    a_1_1 = (
+        two
+        * (x_1[0] - x_2[0])
+        / (
+            x_0[1] * (x_1[0] - x_2[0])
+            - x_0[0] * (x_1[1] - x_2[1])
+            + x_1[1] * x_2[0]
+            - x_1[0] * x_2[1]
+        )
+    )
+
+    jacobian = sa.matrix([[a_0_0, a_0_1], [a_1_0, a_1_1]])
+    return jacobian
+
+
+def transform_to_canonical_jacobian_determinant_2d_triangle_vertex_list(vertex_list):
+    # determinant of jacobian of transformation to canonical element
+    # should be constant scalar as transformation is linear
+    jacobian = transform_to_canonical_jacobian_2d_triangle_vertex_list(vertex_list)
+    det = sa.det(jacobian)
+    return det
+
+
+def transform_to_mesh_2d_triangle_vertex_list(xi, vertex_list):
+    # xi.shape (2, points.shape)
+    # vertex_list.shape = (3, 2)
+    # return shape (2, points.shape)
+
+    # x[0] = b_0_0 xi[0] + b_0_1 xi[1] + b_0_2
+    # x[1] = b_1_0 xi[0] + b_1_1 xi[1] + b_1_2
+    x_0 = vertex_list[0]
+    x_1 = vertex_list[1]
+    x_2 = vertex_list[2]
+    b_0_0 = x_2[0] / two - x_1[0] / two
+    b_0_1 = x_0[0] / two - x_1[0] / two
+    b_0_2 = x_0[0] / two + x_2[0] / two
+    b_1_0 = x_2[1] / two - x_1[1] / two
+    b_1_1 = x_0[1] / two - x_1[1] / two
+    b_1_2 = x_0[1] / two + x_2[1] / two
+    x = sa.vector(
+        [b_0_0 * xi[0] + b_0_1 * xi[1] + b_0_2, b_1_0 * xi[0] + b_1_1 * xi[1] + b_1_2,]
+    )
+    return x
+
+
+def transform_to_mesh_jacobian_2d_triangle_vertex_list(vertex_list):
+    # jacobian of transformation to mesh
+    # should be constant matrix as the transformation is linear
+    # x[0] = b_0_0 xi[0] + b_0_1 xi[1] + b_0_2
+    # x[1] = b_1_0 xi[0] + b_1_1 xi[1] + b_1_2
+    # J = [[b_0_0, b_0_1], [b_1_0, b_1_1]]
+    x_0 = vertex_list[0]
+    x_1 = vertex_list[1]
+    x_2 = vertex_list[2]
+    b_0_0 = x_2[0] / two - x_1[0] / two
+    b_0_1 = x_0[0] / two - x_1[0] / two
+    b_1_0 = x_2[1] / two - x_1[1] / two
+    b_1_1 = x_0[1] / two - x_1[1] / two
+    jacobian = sa.matrix([[b_0_0, b_0_1], [b_1_0, b_1_1]])
+    return jacobian
+
+
+def transform_to_mesh_jacobian_determinant_2d_triangle_vertex_list(vertex_list):
+    # jacobian of transformation to mesh
+    # should be constant scalar as the transformation is linear
+    # x[0] = b_0_0 xi[0] + b_0_1 xi[1] + b_0_2
+    # x[1] = b_1_0 xi[0] + b_1_1 xi[1] + b_1_2
+    # det(J) = b_0_0 * b_1_1 - b_0_1 *b_1_0
+
+    jacobian = transform_to_mesh_jacobian_2d_triangle_vertex_list(vertex_list)
+    det = sa.det(jacobian)
+    return det
